1c242c908e66287608bd8cd31d0cd758bb9fcb8f
João Pedro Pulz <99343129+johnpolsh@users.noreply.github.com>
2024-12-21 15:49:07 -0300

    Refactor PrithviSegmentation to support UNeTRDecoder; implement new segmentation head and enhance backbone handling

--- src/models/components/heads/unetr.py
+++ src/models/components/heads/unetr.py
@@ -1,3 +1,188 @@
 #
 
+import torch
+from torch import nn
+from torch.nn import functional
+from typing import Callable, Literal, Union
 
+
+def intermediate_3d_conv_block(
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple[int, int, int]],
+        stride: Union[int, tuple[int, int, int]] = 1,
+        padding: Union[int, tuple[int, int, int]] = 0,
+        dilation: Union[int, tuple[int, int, int]] = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: Literal['zeros', 'reflect', 'replicate', 'circular'] = 'zeros',
+        eps: float = 1e-05,
+        momentum: float = 0.1,
+        affine: bool = True,
+        act_inplace: bool = True
+        ) -> nn.Sequential:
+    return nn.Sequential(
+        nn.Conv3d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+            padding_mode=padding_mode
+        ),
+        nn.BatchNorm3d(
+            out_channels,
+            eps=eps,
+            momentum=momentum,
+            affine=affine
+            ),
+        nn.ReLU(inplace=act_inplace)
+    )
+
+
+def intermediate_3d_deconv_block(
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple[int, int, int]],
+        stride: Union[int, tuple[int, int, int]] = 1,
+        padding: Union[int, tuple[int, int, int]] = 0,
+        output_padding: Union[int, tuple[int, int, int]] = 0,
+        groups: int = 1,
+        bias: bool = True,
+        dilation: Union[int, tuple[int, int, int]] = 1,
+        eps: float = 1e-05,
+        momentum: float = 0.1,
+        affine: bool = True,
+        act_inplace: bool = True
+        ) -> nn.Sequential:
+    return nn.Sequential(
+        nn.ConvTranspose3d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            output_padding=output_padding,
+            groups=groups,
+            bias=bias,
+            dilation=dilation
+            ),
+        nn.BatchNorm3d(
+            out_channels,
+            eps=eps,
+            momentum=momentum,
+            affine=affine
+            ),
+        nn.ReLU(inplace=act_inplace)
+        )
+
+
+class DeconvBottleneck(nn.Module):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            interm_channels: list[int],
+            deconv_layer: Callable[..., nn.Module] = intermediate_3d_deconv_block,
+            conv_layer: Callable[..., nn.Module] = intermediate_3d_conv_block
+            ):
+        super().__init__()
+        assert len(interm_channels) >= 1, "interm_channels should have at least 1 channel"
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.interm_channels = interm_channels
+
+        deconv_out_features = [in_channels] + interm_channels
+        self.deconv = nn.Sequential(
+            *(deconv_layer(
+                in_channels=deconv_out_features[i],
+                out_channels=deconv_out_features[i+1],
+                kernel_size=(1, 2, 2),
+                stride=(1, 2, 2)
+                ) for i in range(len(deconv_out_features) - 1))
+            )
+        
+        self.conv = nn.Sequential(
+            conv_layer(
+                    in_channels=interm_channels[-1] * 2,
+                    out_channels=interm_channels[-1],
+                    kernel_size=(1, 3, 3),
+                    padding=(0, 1, 1)
+                    ),
+            nn.ConvTranspose3d(
+                in_channels=interm_channels[-1],
+                out_channels=out_channels,
+                kernel_size=(1, 2, 2),
+                stride=(1, 2, 2)
+                )
+            )
+        
+    def forward(self, x: torch.Tensor, prev: torch.Tensor) -> torch.Tensor:
+        x = self.deconv(x)
+        x = torch.cat([x, prev], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class DeconvHeadUNetTR(nn.Module):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            grid_size: tuple[int, int, int],
+            intermediate_out_channels: list[int] = [],
+            bottleneck_layer: Callable[..., nn.Module] = DeconvBottleneck,
+            voxel_reduce: Literal["none", "max", "avg", "conv"] = "conv"
+            ):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.grid_size = grid_size
+        self.intermediate_out_channels = intermediate_out_channels
+        self.voxel_reduce = voxel_reduce
+
+        out_features = intermediate_out_channels + [out_channels]
+        self.deconv0 = nn.ConvTranspose3d(
+            in_channels,
+            out_features[0],
+            kernel_size=(1, 2, 2),
+            stride=(1, 2, 2)
+            )
+        num_layers = len(out_features)
+        conv_out_features = intermediate_out_channels + [out_channels]
+        self.bottleneck = nn.ModuleList([
+            bottleneck_layer(
+                in_channels=in_channels,
+                out_channels=conv_out_features[i+1],
+                interm_channels=intermediate_out_channels[:i+1]
+            ) for i in range(num_layers - 1)
+        ])
+
+        self.conv_reduce = nn.Identity()
+        if voxel_reduce in ["conv"]:
+            self.conv_reduce = nn.Conv3d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=(grid_size[0], 1, 1),
+                stride=(1, 1, 1)
+                )
+
+    def forward(self, x: list[torch.Tensor]) -> torch.Tensor:
+        assert len(x) == len(self.bottleneck) + 1, f"Number of intermediates must be equal to {len(self.bottleneck)}"
+        out = self.deconv0(x[-1])
+        for i, inter in enumerate(reversed(x[:-1])):
+            assert inter.shape[-3] == self.grid_size[0],\
+                f"Expected {inter.shape[-3]=} to be equal to {self.grid_size[0]=}"
+            out = self.bottleneck[i](inter, out)
+        
+        if self.voxel_reduce in ["avg"]:
+            out = torch.mean(out, dim=2)
+        if self.voxel_reduce in ["max"]:
+            out = torch.max(out, dim=2).values
+        if self.voxel_reduce in ["conv"]:
+            out = self.conv_reduce(out).squeeze(2)
+
+        return out
--- src/models/components/prithvi_segmentation.py
+++ src/models/components/prithvi_segmentation.py
@@ -2,10 +2,38 @@
 
 import torch
 from terratorch.datasets import HLSBands
-from terratorch.models.backbones.prithvi_vit import PRETRAINED_BANDS
-from terratorch.models.encoder_decoder_factory import EncoderDecoderFactory
+from terratorch.models.backbones.prithvi_vit import (
+    PRETRAINED_BANDS,
+    prithvi_vit_100
+)
 from torch import nn
-from typing import Literal, Optional
+from typing import Literal, Optional, Union
+from .heads.unetr import DeconvHeadUNetTR
+
+
+_decoder_variants = Literal["FCNDecoder", "UNeTRDecoder"]
+
+
+def _get_segmentation_head(
+        variant: str,
+        num_classes: int,
+        input_embed_dim: int,
+        **kwargs
+        ) -> nn.Module:
+    assert variant in ["FCNDecoder", "UNeTRDecoder"],\
+        f"Got {variant=}, expected one of ['FCNDecoder', 'UNeTRDecoder']"
+    
+    if variant == "FCNDecoder":
+        return nn.Identity()
+    elif variant == "UNeTRDecoder":
+        return DeconvHeadUNetTR(
+            in_channels=input_embed_dim,
+            out_channels=num_classes,
+            intermediate_out_channels=[512, 256, 128],
+            **kwargs
+            )
+    else:
+        raise NotImplementedError(f"Unknown variant {variant=}")
 
 
 class PrithviSegmentation(nn.Module):
@@ -17,7 +45,8 @@ class PrithviSegmentation(nn.Module):
             num_classes: int,
             bands: list[HLSBands] = PRETRAINED_BANDS,
             weights: Optional[Literal["default"]] = "default",
-            decoder: Literal["FCNDecoder"] = "FCNDecoder" # TODO: UNetDecoder
+            decoder: _decoder_variants = "UNeTRDecoder",
+            **kwargs
             ):
         assert weights in ["default"], f"Got {weights=}, expected one of ['default']"
         assert len(bands) == in_chans, f"{bands=} must have {in_chans} elements"
@@ -26,29 +55,40 @@ class PrithviSegmentation(nn.Module):
         self.in_chans = in_chans
         self.num_frames = num_frames
         self.num_classes = num_classes
+        self.decoder = decoder
 
-        self.prithvi_base = EncoderDecoderFactory().build_model(
-            task="segmentation",
-            backbone="prithvi_vit_100",
-            decoder=decoder,
-            num_classes=num_classes,
-            backbone_img_size=img_size,
-            backbone_in_chans=in_chans,
-            backbone_pretrained=bool(weights == "default"),
-            backbone_num_frames=num_frames,
-            backbone_bands=bands,
-            necks=None,
-            rescale=False
-            )
-        self.backbone_params = (
-            list(self.prithvi_base.encoder.parameters())
-            )
-        self.head_params = (
-            list(self.prithvi_base.decoder.parameters()) +
-            list(self.prithvi_base.head.parameters()) +
-            list(self.prithvi_base.aux_heads.parameters())
-            )
+        encoder_kwargs = {
+            "bands": bands,
+            "weights": weights in ["default"],
+            "img_size": img_size,
+            "in_chans": in_chans,
+            "num_frames": num_frames,
+            "features_only": True
+        }
+
+        if decoder in ["UNeTRDecoder"]:
+            encoder_kwargs["out_indices"] = (2, 5, 8, 11)
+        self.backbone = prithvi_vit_100(**encoder_kwargs)
+
+        decoder_kwargs = {
+            "variant": decoder,
+            "num_classes": num_classes,
+            "input_embed_dim": self.backbone.embed_dim,
+        }
+        if decoder in ["UNeTRDecoder"]:
+            decoder_kwargs["grid_size"] = self.backbone.patch_embed.grid_size
+            decoder_kwargs["voxel_reduce"] = kwargs.get("voxel_reduce", "conv")
+        self.head = _get_segmentation_head(**decoder_kwargs)
         
+    def forward_backbone(self, x: torch.Tensor) -> torch.Tensor:
+        return self.backbone(x)
+
+    def forward_head(self, x: Union[torch.Tensor, list[torch.Tensor]]) -> torch.Tensor:
+        if self.decoder in ["UNeTRDecoder"]:
+            B, _, L = x[0].shape
+            F, H, W = self.backbone.patch_embed.grid_size
+            x = [y[:, 1:].transpose(1, 2).view(B, L, F, H, W) for y in x]
+        return self.head(x)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         _, C, F, H, W = x.shape
@@ -59,7 +99,7 @@ class PrithviSegmentation(nn.Module):
         assert F == self.num_frames,\
             f"Expected {F=} to be equal to {self.num_frames=}"
         
-        result = self.prithvi_base(x)
-        logits = result.output
+        embeddings = self.forward_backbone(x)
+        out = self.forward_head(embeddings)
 
-        return logits
+        return out
